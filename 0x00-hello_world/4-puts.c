9a5835bad422642f60c01f9be9e58df8c22dde55
root <jvava911@gmail.com>
2023-08-10 20:38:01 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
- * main - A program that prints a line with puts function
- * Return: 0 (Success)
- */
- int main(void)
+
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-returin(0);
+    puts("\nProgramming is like building a multilingual puzzle,");
+    return 0;
 }
+
