6c3056c7b48086a0ee8bd67fd6deb824a72a7f16
root <jvava911@gmail.com>
2023-08-10 21:05:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the standard error
+ * Return 1 (sucess)
+ */
+int main(void)
+{
+fput("and that pece of art is userful\" , DOra korpar, 215-10-19\r", stdout);
+return (1);
+}
