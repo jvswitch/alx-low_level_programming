beb487c11be87c655dcce35789c59d5c4b53eafa
root <jvava911@gmail.com>
2023-08-13 08:42:15 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,32 @@
+#include <stdio.h>
 #include <unistd.h>
 
-void print_digit(int num) {
-	write(1, &num, 1);
+/**
+ * print_digit - Function to print a digit character
+ * @num: The digit to print
+ */
+void print_digit(int num)
+{
+write(1, &num, 1);
 }
-
-void print_combinations(void) {
+/**
+ * print_combinations - Function to print all combinations of two digits
+ */
+void print_combinations(void)
+{
 	int i, j;
 
-	for (i = 0; i < 9; i++) {
-		for (j = i + 1; j <= 9; j++) {
-			print_digit(i + '0');
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+		print_digit(i + '0');
 			print_digit(j + '0');
 
-			if (i != 8 || j != 9) {
-				if (i != 8 || j != 9 - 1) {
+			if (i != 8 || j != 9)
+			{
+				if (i != 8 || j != 9 - 1)
+				{
 					print_digit(',');
 					print_digit(' ');
 				}
@@ -22,7 +35,12 @@ void print_combinations(void) {
 	}
 }
 
-int main(void) {
-	print_combinations();
-	return (0);
+/**
+ * main - Entry point, invokes print_combinations function
+ * Return: Always 0
+ */
+int main(void)
+{
+print_combinations();
+return (0);
 }
