620f81895649d7380f5d6d019faea433ef8d9c9e
root <jvava911@gmail.com>
2023-08-13 07:40:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - print three number combination
+ * main - function to print combination of two numbers
  *
- * Return : 0 success
+ * Return : 0 on success
  */
 
-int main(void)
-{
-	int i = '0';
-	int j = '0';
-	int k = '0';
+void putchar(char c);
 
-	while(i < '7')
-	{
-		while (j <= '8')
-		{
-			while (k <= '9')
-			{
-				if (i < j && j < k)
-				{
-					putcahr(i);
-					putchar(j);
-					putchar(k);
-					if (!(i == '7' && j == '8' && k == '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				k++;
-			}
-			k = '0';
-			j++;
-		}
-		j = '0'
-		i++;
-	}
-	putchar('\n');
-	return (0);
+void print_combinations(void) {
+    int i, j;
+
+    for (i = 0; i < 9; i++) {
+        for (j = i + 1; j <= 9; j++) {
+            putchar(i + '0');
+            putchar(j + '0');
+
+            if (i != 8 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 }
+
+int main(void) {
+    print_combinations();
+    return (0);
+}
+
