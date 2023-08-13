2e59f5526d42ea7126255cb5616db332eccad232
root <jvava911@gmail.com>
2023-08-13 11:18:02 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
- * print_digit - Function to print a digit character
- * @num: The digit to print
- */
-void print_digit(int num)
-{
-write(1, &num, 1);
-}
-
-/**
- * print_combinations - Function to print all combinations of two digits
- */
-void print_combinations(void)
-{
-int i, j;
-
-for (i = 0; i < 9; i++)
-{
-	for (j = i + 1; j <= 9; j++)
-	{
-		print_digit(i + '0');
-		print_digit(j + '0');
-
-		if (i != 8 || j != 9)
-		{
-			if (i != 8 || j != 9 - 1)
-			{
-				print_digit(',');
-				print_digit(' ');
-			}
-		}
-	}
-}
-
-/**
- * main - Entry point, invokes print_combinations function
- * Return: Always 0
+ * main - print two digits separated with , and space
+ *
+ * Return: (0)
+ *
  */
 int main(void)
 {
-print_combinations();
-return (0);
+	int num_1, num_2;
+
+	num_1 = '0';
+	num_2 = '0';
+
+	while (num_1 <= '9')
+	{
+		while (num_2 <= '9')
+		{
+			if (num_1 < num_2)
+			{
+				putchar(num_1);
+				putchar(num_2);
+				if (num_1 != '8' || (num_1 == '8' && num_2 != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num_2++;
+		}
+		num_1++;
+		num_2 = '0';
+	}
+	putchar('\n');
+	return (0);
 }
