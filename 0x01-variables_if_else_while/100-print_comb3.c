43f741a5dcb54322d57d9679345d8c94ad92a5a9
root <jvava911@gmail.com>
2023-08-13 08:54:40 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
  */
 void print_digit(int num)
 {
-    write(1, &num, 1);
+write(1, &num, 1);
 }
 
 /**
@@ -15,25 +15,24 @@ void print_digit(int num)
  */
 void print_combinations(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            print_digit(i + '0');
-            print_digit(j + '0');
+for (i = 0; i < 9; i++)
+{
+	for (j = i + 1; j <= 9; j++)
+	{
+		print_digit(i + '0');
+		print_digit(j + '0');
 
-            if (i != 8 || j != 9)
-            {
-                if (i != 8 || j != 9 - 1)
-                {
-                    print_digit(',');
-                    print_digit(' ');
-                }
-            }
-        }
-    }
+		if (i != 8 || j != 9)
+		{
+			if (i != 8 || j != 9 - 1)
+			{
+				print_digit(',');
+				print_digit(' ');
+			}
+		}
+	}
 }
 
 /**
@@ -42,6 +41,6 @@ void print_combinations(void)
  */
 int main(void)
 {
-    print_combinations();
-    return (0);
+print_combinations();
+return (0);
 }
