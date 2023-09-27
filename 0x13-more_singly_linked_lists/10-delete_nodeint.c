c72c259ce88f06c31c3e62ea13d3060dce6217f0
jvswitch <jvava911@gmail.com>
2023-09-27 00:34:55 -0700

    updated 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,63 +1,38 @@
 #include "lists.h"
 /**
- * create_new_node - creates a new node
- * @n: the data of the node
- * Return: pointer to the node
+ * delete_nodeint_at_index - delete a node of a list at
+ * a paticular index
+ * @head: pointer to the beginning of the list
+ * @index: index of the node to be freed
+ * Return: 1 if successful an -1 if not
  */
-listint_t *create_new_node(int n)
-{
-	listint_t *new_node;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-
-	return (new_node);
-}
-
-/**
- * insert_nodeint_at_index - insert a node at an index in a list
- * @head: pointer to the first element of the list
- * @idx: index to the position the new node is to be inserted
- * @n: the data of the new node created
- * Return: the address of the newly created node
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *tmp;
-	listint_t *tmp_old;
-	listint_t *new_node;
+	listint_t *node;
 
 	tmp = *head;
-	if (head == NULL)
-		return (NULL);
-	new_node = create_new_node(n);
-	if (new_node == NULL)
-		return (NULL);
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	if (idx == 0)
-	{
-		*head = new_node;
-	}
-	for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
+	if (head == NULL || *head == NULL)
+		return (-1);
+	for (i = 0; i < index - 1 && tmp != NULL && index != 0; i++)
 		tmp = tmp->next;
 	if (tmp == NULL)
-		return (NULL);
-	if (idx == 0)
-		new_node->next = tmp;
+		return (-1);
+	if (index == 0)
+	{
+		node = tmp->next;
+		free(tmp);
+		*head = node;
+	}
 	else
 	{
-		tmp_old = tmp->next;
-		tmp->next = new_node;
-		new_node->next = tmp_old;
+		if (tmp->next == NULL)
+			node = tmp->next;
+		else
+			node = tmp->next->next;
+		free(tmp->next);
+		tmp->next = node;
 	}
-	return (new_node);
+	return (1);
 }
